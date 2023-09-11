6db3f032b8e6fcda74c0a5ef374e614334660c86
shug <vxzyfx@gmail.com>
2023-09-11 08:55:31 +0800

    fix: jaon

--- nvim/lua/lsp/config.lua
+++ nvim/lua/lsp/config.lua
@@ -1,71 +1,66 @@
-local yaml_extra = {
+local extra = {
   {
     description = "coding net cli",
-    fileMatch = {".coding-ci.yml"},
+    fileMatch =  { ".coding-ci.yml" },
     name = "conding.yml",
     url = "https://ci.coding.net/docs/conf-schema.json",
   },
 }
 
-local yaml_schemas = {
-   ["https://ci.coding.net/docs/conf-schema.json"] = "/.coding-ci.yml",
-}
 return {
   {
     "neovim/nvim-lspconfig",
+    optional = true,
     dependencies = {
       "b0o/SchemaStore.nvim",
     },
-    opts = {
-      yamlls = {
-        capabilities = {
-          textDocument = {
-            foldingRange = {
-              dynamicRegistration = false,
-              lineFoldingOnly = true,
+    opts = function(_, opts)
+      local schemastore = require("schemastore");
+      return {
+        yamlls = {
+          capabilities = {
+            textDocument = {
+              foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true,
+              },
+            },
+          },
+          settings = {
+            redhat = { telemetry = { enabled = false } },
+            yaml = {
+              keyOrdering = false,
+              format = {
+                enable = true,
+              },
+              hover = true,
+              completion = true,
+              validate = true,
+              schemas = schemastore.yaml.schemas {
+                extra = extra,
+              },
+              schemaStore = {
+                enable = false,
+                url = "https://www.schemastore.org/api/json/catalog.json",
+              },
             },
           },
         },
-        -- lazy-load schemastore when needed
-        on_new_config = function(new_config)
-          new_config.settings.yaml.schemas = new_config.settings.yaml.schemas or {}
-          vim.list_extend(new_config.settings.yaml.schemas, require("schemastore").yaml.schemas {
-          })
-        end,
-        settings = {
-          redhat = { telemetry = { enabled = false } },
-          yaml = {
-            keyOrdering = false,
-            format = {
-              enable = true,
-            },
-            hover = true,
-            completion = true,
-            validate = true,
-            schemaStore = {
-              enable = true,
-              url = "https://www.schemastore.org/api/json/catalog.json",
+        jsonls = {
+          settings = {
+            json = {
+              format = {
+                enable = true,
+              },
+              schemas = schemastore.json.schemas {
+                extra = extra,
+              },
+              validate = { enable = true },
             },
           },
-        },
-      },
-      jsonls = {
-          -- lazy-load schemastore when needed
-        on_new_config = function(new_config)
-            new_config.settings.json.schemas = new_config.settings.json.schemas or {}
-            vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas {
-          })
-        end,
-        settings = {
-          json = {
-            format = {
-              enable = true,
-            },
-            validate = { enable = true },
-          },
-        },
-      },
-    },
+        }
+      }
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
