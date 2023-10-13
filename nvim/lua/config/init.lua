adce4ed8b7b831bc56d7a72727f7f99b32ce56b0
vxshug <vxzyfx@gmail.com>
2023-10-13 18:17:12 +0800

    feat: use env

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,8 +1,12 @@
 require("config.options")
 require("config.autocommands")
-vim.g.tabby_server_url = "http://127.0.0.1:6666"
 vim.g.tabby_accept_binding = "<C-j>"
 vim.g.tabby_dismiss_binding = "<C-k>"
+
+local tabby_server_url = os.getenv("TABBY_SERVER_URL")
+if tabby_server_url then
+  vim.g.tabby_server_url = tabby_server_url
+end
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
